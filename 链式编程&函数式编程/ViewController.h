d97de0e5870041fffc833630aca93f77cfcb6800
浅爱 <shallowlove21@gmail.com>
2017-12-22 14:05:51 +0800

    Initial Commit

---
+++ 链式编程&函数式编程/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  链式编程&函数式编程
+//
+//  Created by PatrickY on 2017/12/22.
+//  Copyright © 2017年 PatrickY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 链式编程&函数式编程/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  链式编程&函数式编程
+//
+//  Created by PatrickY on 2017/12/22.
+//  Copyright © 2017年 PatrickY. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
